a8d1dc6a1f202b78bae8fd591733ca54af788a2c
claytoneduard <clayton_eduard@hotmail.com>
2022-06-25 18:21:10 -0300

    feat: implementação do salvar carro e update carro

--- src/main/java/com/br/garagem/cars/CarService.java
+++ src/main/java/com/br/garagem/cars/CarService.java
@@ -2,6 +2,7 @@ package com.br.garagem.cars;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -32,6 +33,52 @@ public class CarService {
             ;
         });
         return carList;
+    }
+
+    // buscar carro por id
+    public Optional<Car> getCarByID(Integer id) {
+        return repo.findById(id);
+    }
+
+    public String saveNewCar(String placa, String modelo) {
+        if (placa != null) {
+            return "Erro: O campo placa é obrigatório!";
+        }
+        if ((placa.length() != 8) || (!placa.contains("-"))) {
+            return "Erro: A placa deve conter o padrão XXX-9999";
+        }
+
+        Car car = new Car();
+        car.setPlaca(placa);
+        car.setModelo(modelo);
+
+        repo.save(car);
+        return "Sucesso: Cadastro concluído!";
+    }
+
+    public String updateCar(Integer id, String placa, String modelo) {
+
+        if (placa != null) {
+            return "Erro: O campo placa é obrigatório!";
+        }
+        if ((placa.length() != 8) || (!placa.contains("-"))) {
+            return "Erro: A placa deve conter o padrão XXX-9999";
+        }
+
+        Car car = new Car();
+        Boolean carExist = repo.existsById(id);
+
+        if (!carExist) {
+            return "Erro: Carro não encontrado!";
+        }
+        Optional<Car> oldCar = repo.findById(id);
+        car.setId(id);
+        car.setData_entrada(oldCar.get().getData_entrada());
+        car.setPlaca(placa);
+        car.setModelo(modelo);
+        car.setData_saida(oldCar.get().getData_saida());
+        repo.save(car);
+        return "Sucesso: Alteração concluída!";
     }
 
 }
