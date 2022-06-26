3d30e84960b5dcb02f0234fde961a47d3d60b7b0
claytoneduard <clayton_eduard@hotmail.com>
2022-06-26 01:41:34 -0300

    feat: Implementação do metodo post com a view

--- src/main/java/com/br/garagem/cars/CarController.java
+++ src/main/java/com/br/garagem/cars/CarController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CarController {
@@ -17,28 +19,44 @@ public class CarController {
 
    // lista de carros que estao estacionados
    @GetMapping("/parking-in")
-   public String showParketStopPage(Model model) {
+   public String showParkedStopPage(Model model) {
       List<Car> listCars = service.listParkedCars();
       model.addAttribute("listCars", listCars);
       return "parking-in";
    }
 
+   @PostMapping("/new-car")
+   public String saveNewCar(@ModelAttribute Car car, Model model) {
+      model.addAttribute("car", car);
+      String message = service.saveNewCar(car.getPlaca(), car.getModelo());
+      if (message.contains("Erro")) {
+         model.addAttribute("Erro", message);
+      }
+      ;
+      return "new-car";
+   }
+
+   @GetMapping("/new-car")
+   public String showNewCarForm() {
+      return "new-car";
+   }
+
    @GetMapping("/parking-out")
-   public String showParketOutPage(Model model) {
+   public String showParkedOutPage(Model model) {
       List<Car> listCars = service.listCarsOut();
       model.addAttribute("listCars", listCars);
       return "parking-out";
    }
 
    @GetMapping("/edit-car/{id}")
-   public String showEditform(Model model,@PathVariable("id") Integer id) {
+   public String showEditForm(Model model, @PathVariable("id") Integer id) {
       Optional<Car> car = this.service.getCarByID(id);
       model.addAttribute("car", car);
       return "edit-car";
    }
 
    @GetMapping("/out-car/{id}")
-   public String showOutform(Model model, @PathVariable("id") Integer id) {
+   public String showOutForm(Model model, @PathVariable("id") Integer id) {
       Optional<Car> car = this.service.getCarResume(id);
       model.addAttribute("car", car);
       return "out-car";
